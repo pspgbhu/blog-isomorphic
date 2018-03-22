17d4b3f2812a7c453e8325dcc6ba99148b8614d1
hucong <hucong@jd.com>
2018-03-22 17:18:39 +0800

    fix: del 备案

--- common/components/Footer.jsx
+++ common/components/Footer.jsx
@@ -7,10 +7,7 @@ const Footer = props => (
         <div className="row">
             <div className="col-sm-12">
                 <span>Copyright © 2017
-                </span> |
-                <span>
-                  {/* <a className="copyright-links" target="_blank" rel="nofollow" href="http://www.miitbeian.gov.cn/">陕ICP备17007121号</a> */}
-                </span> |
+                </span>|
                 <span>
                     <a href="//pspgbhu.me" className="copyright-links" rel="nofollow">Pspgbhu's Site</a>
                 </span>
