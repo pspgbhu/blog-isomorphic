29d662fb876a4cb2f07619f18c23063048fbf820
hucong <hucong@jd.com>
2018-03-28 02:10:54 +0800

    chore: webpack deal image

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -31,6 +31,30 @@ const config = {
           cacheDirectory: true,
         },
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'img/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'media/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[ext]',
+        },
+      },
     ],
   },
 
