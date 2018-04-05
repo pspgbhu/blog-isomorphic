523c535eaef62fa08fea6ea1a49087a520f48147
hucong <hucong@jd.com>
2018-04-06 02:31:16 +0800

    refactor: 页面 Title 设置相关代码重构

--- client/Title.jsx
+++ client/Title.jsx
@@ -22,44 +22,36 @@ class Title extends Component {
       return;
     }
 
-    // 文章
-    if (this.props.location.pathname.match(/^\/article\//)) {
-      const rst = this.props.location.pathname.match(/\/article\/([\w-]*)/);
-      if (!rst || !rst[1]) return;
+    const pages = [{
+      regExp: new RegExp(/\/article\/([\w-]*)/), // article
+      title: (rst) => {
+        const { title } = this.props.posts[rst[1]];
+        return title ? `${title} | Pspgbhu 的博客` : 'Pspgbhu 的博客';
+      },
+    }, {
+      regExp: new RegExp(/\/categories\/([\w-]*)/), // categories
+      title: rst => `分类：${rst[1]} | Pspgbhu 的博客`,
+    }, {
+      regExp: new RegExp(/^\/archives\/(\d+(?:\/\d*))/), // archives
+      title: rst => `归档：${rst[1]} | Pspgbhu 的博客`,
+    }, {
+      regExp: new RegExp(/\/tags\/([\w-]*)/), // tags
+      title: rst => `标签：${rst[1]} | Pspgbhu 的博客`,
+    }];
 
-      const { title } = this.props.posts[rst[1]];
+    let done = false;
+    pages.forEach(({ regExp, title }) => {
+      if (done) return;
 
-      if (title) {
-        document.title = `${title} | Pspgbhu 的博客`;
+      const rst = this.props.location.pathname.match(regExp);
+      if (!rst) return;
+
+      const tit = title(rst);
+      if (tit) {
+        document.title = tit;
       }
-      return;
-    }
-
-    // 分类
-    if (this.props.location.pathname.match(/^\/categories\//)) {
-      const rst = this.props.location.pathname.match(/\/categories\/([\w-]*)/);
-      if (!rst || !rst[1]) return;
-
-      document.title = `分类：${rst[1]} | Pspgbhu 的博客`;
-      return;
-    }
-
-    // 归档
-    if (this.props.location.pathname.match(/^\/archives\//)) {
-      const rst = this.props.location.pathname.match(/^\/archives\/(\d+(?:\/\d*))/);
-      if (!rst || !rst[1]) return;
-
-      document.title = `归档：${rst[1]} | Pspgbhu 的博客`;
-      return;
-    }
-
-    // 标签
-    if (this.props.location.pathname.match(/^\/tags\//)) {
-      const rst = this.props.location.pathname.match(/\/tags\/([\w-]*)/);
-      if (!rst || !rst[1]) return;
-
-      document.title = `标签：${rst[1]} | Pspgbhu 的博客`;
-    }
+      done = true;
+    });
   }
 
   render() {
