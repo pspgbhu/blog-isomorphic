b507c57abd615eec5e102063e24da8203b2bcfcd
pspgbhu <brotherchun001@gmail.com>
2018-05-05 12:44:09 +0800

    style: 样式修改

--- common/pages/Projects/index.jsx
+++ common/pages/Projects/index.jsx
@@ -11,7 +11,7 @@ const list = [
       'http://static.zybuluo.com/pspgbhu/r46m2gwpp2b9aabhnbc84elp/vue-stone.png',
   }, {
     name: 'React Isomorphic',
-    desc: 'React 同构直出工程脚手架',
+    desc: 'React 同构直出工程脚手架。',
     link: 'https://github.com/pspgbhu/react-isomorphic',
     image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520541763181&di=5421f1ab7a5b72340d02b021493d244b&imgtype=0&src=http%3A%2F%2Fs1.51cto.com%2Fwyfs02%2FM01%2F88%2F7F%2FwKiom1f55HCSS-DrAACSkyHme8o914.png-wh_651x-s_1436211364.png',
   }, {
