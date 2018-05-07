a9eba287a5cd6907e0768a4ba15bf85bf4a70bad
pspgbhu <brotherchun001@gmail.com>
2018-05-08 00:00:33 +0800

    feat: Footer 中添加 Powered by Rephic

--- common/components/Footer.jsx
+++ common/components/Footer.jsx
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types';
 const Footer = props => (
   <div className="copyright">
     <div className="container">
-        <div className="row">
-            <div className="col-sm-12">
-                <span>Copyright © 2017-2018
+      <div className="row">
+        <div className="col-sm-12">
+          <span>Copyright © 2017-2018
                 </span>|
                 <span>
-                    <a href="//pspgbhu.me" className="copyright-links" rel="nofollow">Pspgbhu's Site</a>
-                </span>
-            </div>
+            <a href="//pspgbhu.me" className="copyright-links" rel="nofollow">Pspgbhu's Site</a>
+          </span>
         </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12">
+          Powered by <a href="https://github.com/pspgbhu/rephic">Rephic</a>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 );
 
 Footer.propTypes = {
