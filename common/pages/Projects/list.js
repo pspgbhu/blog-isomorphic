d1b31099623dc4a56f8d80cc5e87f78d397363c3
pspgbhu <brotherchun001@gmail.com>
2018-05-15 03:07:02 +0800

    feat: 新增 open source project

---
+++ common/pages/Projects/list.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    name: 'Vue Stone',
+    desc: '基于 Vue 的移动端 UI 组件库。',
+    link: 'https://github.com/jd-smart-fe/vue-stone',
+    image:
+      'http://static.zybuluo.com/pspgbhu/r46m2gwpp2b9aabhnbc84elp/vue-stone.png',
+  }, {
+    name: 'React Isomorphic',
+    desc: 'React 同构直出工程脚手架。',
+    link: 'https://github.com/pspgbhu/react-isomorphic',
+    image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520541763181&di=5421f1ab7a5b72340d02b021493d244b&imgtype=0&src=http%3A%2F%2Fs1.51cto.com%2Fwyfs02%2FM01%2F88%2F7F%2FwKiom1f55HCSS-DrAACSkyHme8o914.png-wh_651x-s_1436211364.png',
+  }, {
+    name: 'C Swipe',
+    desc: '一款轻量级 Vue2 轮播组件。',
+    link: 'https://github.com/pspgbhu/vue-swipe-mobile',
+    image: 'https://raw.githubusercontent.com/pspgbhu/pspgbhu.github.io/master/assets/img/cswipe-demo.gif',
+  }, {
+    name: 'GitHub Download Parts',
+    desc: '具备下载 GitHub 仓库中部分文件夹的能力',
+    link: 'https://github.com/pspgbhu/github-download-parts',
+    image: 'http://static.zybuluo.com/pspgbhu/8a6qm5mqh12o960090d4uide/github.png',
+  },
+];
